e775fcb85f106d922c6d0e0fcfe80622b82dfa60
Jeremiah Lant <jlant@usgs.gov>
2015-03-27 13:46:55 -0400

    created favorite movies website in lesson3

--- lesson3/movies/entertainment_center.py
+++ lesson3/movies/entertainment_center.py
@@ -1,21 +1,36 @@
+import fresh_tomatoes
 import media
-
-toy_story = media.Movie(movie_title = "Toy Story",
-						movie_storyline = "A story of a boy and his toys that come to life.", 
-						poster_image = "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", 
-						trailer_youtube = "https://www.youtube.com/watch?v=KYz2wyBy3kc")
-print(toy_story.storyline)
-
-avatar = media.Movie(movie_title = "Avatar",
-					 movie_storyline = "A marine on an alien planet.", 
-					 poster_image = "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg", 
-					 trailer_youtube = "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
-print(avatar.storyline)
 
 matrix = media.Movie(movie_title = "The Matrix",
 					 movie_storyline = "Neo searches for the truth about the Matrix and discovers his place in it.", 
 					 poster_image = "http://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg", 
 					 trailer_youtube = "https://www.youtube.com/watch?v=_Ls19O-9p3s")
-matrix.show_trailer()
+
+point_break = media.Movie(movie_title = "Point Break",
+					     movie_storyline = "Reeves stars as rookie FBI agent Johnny Utah, who is investigating a string of bank robberies possibly being committed by surfers. Johnny goes undercover to infiltrate the surfing community and develops a complex friendship with Bodhi (Swayze), the charismatic leader of a gang of surfers.", 
+					     poster_image = "http://upload.wikimedia.org/wikipedia/en/7/7e/Pointbreaktheatrical.jpg", 
+					     trailer_youtube = "https://www.youtube.com/watch?v=AVk3mR2UhgI")
+
+fight_club = media.Movie(movie_title = "Fight Club",
+					     movie_storyline = "An insomniac office worker looking for a way to change his life crosses paths with a devil-may-care soap maker and they form an underground fight club that evolves into something much, much more...", 
+					     poster_image = "http://upload.wikimedia.org/wikipedia/en/f/fc/Fight_Club_poster.jpg", 
+					     trailer_youtube = "www.youtube.com/watch?v=SUXWAEX2jlg")
+
+usual_suspects = media.Movie(movie_title = "The Usual Suspects",
+						     movie_storyline = "A sole survivor tells of the twisty events leading up to a horrific gun battle on a boat, which begin when five criminals meet at a seemingly random police lineup.", 
+						     poster_image = "http://upload.wikimedia.org/wikipedia/en/9/9c/Usual_suspects_ver1.jpg", 
+						     trailer_youtube = "https://www.youtube.com/watch?v=oiXdPolca5w")
+
+apollo = media.Movie(movie_title = "Apollo 13",
+					 movie_storyline = "The film depicts astronauts Lovell, Jack Swigert and Fred Haise aboard Apollo 13 for America's third Moon landing mission. En route, an on-board explosion deprives their spacecraft of most of its oxygen supply and electric power, forcing NASA's flight controllers to abort the Moon landing, and turning the mission into a struggle to get the three men home safely.", 
+					 poster_image = "http://upload.wikimedia.org/wikipedia/en/9/9e/Apollo_thirteen_movie.jpg", 
+					 trailer_youtube = "https://www.youtube.com/watch?v=nEl0NsYn1fU")
+
+contact = media.Movie(movie_title = "Contact",
+					  movie_storyline = "A SETI scientist who finds strong evidence of extraterrestrial life and is chosen to make first contact.", 
+					  poster_image = "http://upload.wikimedia.org/wikipedia/en/7/75/Contact_ver2.jpg", 
+					  trailer_youtube = "https://www.youtube.com/watch?v=d9C2cF3KvP8")
 
 
+movies = [matrix, point_break, fight_club, usual_suspects, apollo, contact]
+fresh_tomatoes.open_movies_page(movies)
